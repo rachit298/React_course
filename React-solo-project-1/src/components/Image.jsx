57d1ab23639280dbdb23e2e9b62bc6a012926fbe
Rachit Srivastava <69860596+rachit298@users.noreply.github.com>
2023-01-26 10:38:36 +0530

    Delete React-solo-project-1 directory

--- React-solo-project-1/src/components/Image.jsx
+++
@@ -1,5 +0,0 @@
-import profile from "../assets/id.jpg";
-
-export default function Image() {
-  return <img className="id" src={profile}/>;
-}
--- React-solo-project-1/src/components/Name.jsx
+++
@@ -1,6 +0,0 @@
-export default function Name() {
-    return <div className="name-and-role">
-        <h2 className="fullname">Rachit Srivastava</h2>
-        <p className="role">Frontend Developer</p>
-    </div>
-}
